d47a246fa3d37f82bbb2b9de5cacc7262658dc8e
Michael Wilson <axodevelopment@icloud.com>
2026-05-01 03:02:41 -0400

    registering missed timer for micrometer in springboot

--- src/main/java/com/training/Application.java
+++ src/main/java/com/training/Application.java
@@ -1,13 +1,22 @@
 package com.training;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
     }
 }
